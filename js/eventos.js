894a5fb900120d8bb0ef1fde3e47e6b9894895c0
davidbgemin7357 <davidbgemin.dev@gmail.com>
2022-08-13 18:48:49 -0500

    carrusel 1

--- js/eventos.js
+++ js/eventos.js
@@ -1,34 +1,51 @@
-// eventos del carrusel (pendiente)
+const openModal = document.querySelectorAll(".mostrarFotoProy");
+const modal = document.querySelector(".modal");
+const closeModal = document.querySelector(".modal__close");
+const imagenes = document.querySelectorAll(".swiper-slide");
 
-const indIma = [
-  "http://i2.cdn.turner.com/money/dam/assets/130423094442-tesla-model-s-620xa.jpg",
-  "https://cnnespanol.cnn.com/wp-content/uploads/2021/11/oso-Bart.jpg?quality=100&strip=info&w=940&h=530&crop=1",
-  "https://www.purina-latam.com/sites/g/files/auxxlc391/files/styles/kraken_generic_max_width_960/public/Purina%C2%AE%20Como%20disciplinar%20a%20tu%20gato.jpg?itok=Nxydhxbv",
-  "https://www.caracteristicas.co/wp-content/uploads/2017/02/perro-3-e1561679226953.jpg",
-  "https://imagenes.elpais.com/resizer/RJqQIHBxgS3ZfDJrVSRkmhGAX9g=/980x0/cloudfront-eu-central-1.images.arcpublishing.com/prisa/2KZAO67QVMMA2FCPSL3XR3DABM.jpg",
-  "https://www.treehugger.com/thmb/MEcPnD73YGv980Zz3FAW_ymketA=/768x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-1043597638-49acd69677d7442588c1d8930d298a59.jpg",
-  "https://files.worldwildlife.org/wwfcmsprod/images/African_Elephant_Walking/story_carousel/3arzfzjsw0_Medium_WW1113482.jpg"
-]
+let i = Object.keys(openModal);
 
-for (let i=0; i<) {
-    const openModal = document.querySelectorAll(".mostrarModal");
-    const modal = document.querySelector(".modal");
-    const closeModal = document.querySelector(".modal__close");
+const pictures = [
+    "../assets/img/FOTO11_Mantenimiento de instalaciones de FCR.jpeg",
+    "../assets/img/FOTO11_Instalacion panel led en FCR.jpeg",
+    "../assets/img/FOTO 9_levantamiento de información en tableros electricos para  diagramas unif.jpg",
+    "../assets/img/FOTO 10_llenado de check list de diagnostico para lev de info tableros.jpeg",
+    "../assets/img/subestacion-1.jpg",
+    "../assets/img/FOTO 1_Mantenimiento de tableros electricos.jpg",
+    "../assets/img/FOTO 2_mantenimiento de tableros electricos.jpg",
+    "../assets/img/FOTO 5_picado para malla de SPAT.jpg",
+    "../assets/img/FOTO 6_nuevo pozo a tierra.jpg",
+    "../assets/img/FOTO11_Mantenimiento de instalaciones de FCR.jpeg",
+    "../assets/img/FOTO11_Instalacion panel led en FCR.jpeg",
+    "../assets/img/FOTO12_Mantenimiento Y Soporte RED.jpeg",
+    "../assets/img/FOTO13_Mant y soporte RED.jpeg",
+];
 
-    let divImage = document.querySelector(".swiper-wrapper");
-    const image = document.createElement("img");
-
-    openModal[property].addEventListener("click", (e) => {
-        image.src = `${indIma[property]}`;
-            
-        image.classList.add("swiper-slide");
-        divImage.append(image);
+const mostrar = (indice, p1, p2) => {
+    openModal[indice].addEventListener("click", (e) => {
+        imagenes[0].src = `${p1}`;
+        imagenes[1].src = `${p2}`;
         e.preventDefault();
         modal.classList.add("modal--show");
     });
+};
 
-    closeModal.addEventListener("click", (e) => {
-        e.preventDefault();
-        modal.classList.remove("modal--show");
-    });
-}
+mostrar(0, pictures[0], pictures[1]);
+mostrar(1, pictures[2], pictures[3]);
+mostrar(2, pictures[4], pictures[4]);
+mostrar(3, pictures[5], pictures[6]);
+mostrar(4, pictures[7], pictures[8]);
+mostrar(5, pictures[9], pictures[10]);
+mostrar(6, pictures[11], pictures[12]);
+
+closeModal.addEventListener("click", (e) => {
+    e.preventDefault();
+    modal.classList.remove("modal--show");
+});
+
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
